81d63a6b3e018b2fe458c52440dfe7b3a40efb85
Carlos Siqueri <carlossiqueri@Carlos>
2023-05-31 10:25:53 -0300

    fix: deploy bug with empty import

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,8 @@
-import { Router } from "express"
-import authRouter from "./auth.routes.js"
-import userRouter from "./user.routes.js"
+import { Router } from "express";
+import userRouter from "./user.routes.js";
 
-const router = Router()
-router.use(authRouter) 
-router.use(userRouter)
+const router = Router();
+router.use(authRouter);
+router.use(userRouter);
 
-export default router
+export default router;
