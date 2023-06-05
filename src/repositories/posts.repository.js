3fff361c8a1435e1043abe022beb4daabef8748b
Carlos Siqueri <carlossiqueri@Carlos>
2023-06-04 22:15:16 -0300

    fix: trys to fix api deploy

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -16,32 +16,34 @@ export function createPost(url, description, user_id, urlMeta, url_picture) {
   );
 }
 
+
+
 export function getPostsDB() {
   return db.query(
-    `
-  SELECT posts.*, users.username, users.picture_url AS user_picture
-  FROM users, posts_hashtags, users
-  JOIN posts ON users.id = user_id
-  ORDER BY id DESC
-  LIMIT 20;
-  `,
-    []
+`
+SELECT posts.*, u.username, u.picture_url AS user_picture
+FROM posts
+JOIN users AS u ON u.id = posts.user_id
+ORDER BY posts.id DESC
+LIMIT 20;
+`
   );
 }
 
-
-export function insertHashtagDB(hashtag){
-  return (
-    db.query(`
+export function insertHashtagDB(hashtag) {
+  return db.query(
+    `
   INSERT INTO hashtags(hashtag_name) VALUES ($1) RETURNING id;
-  `, [hashtag])
-  )
+  `,
+    [hashtag]
+  );
 }
 
-export function insert_posts_hashtagsDB(hashtag_id, post_id){
-  return(
-    db.query(`
+export function insert_posts_hashtagsDB(hashtag_id, post_id) {
+  return db.query(
+    `
       INSERT INTO posts_hashtags (hashtag_id, post_id) VALUES ($1, $2)
-    `, [hashtag_id, post_id])
-  )
+    `,
+    [hashtag_id, post_id]
+  );
 }
